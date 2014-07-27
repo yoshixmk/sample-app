a0eecb50f848a2c66bf3bc36672bd75e280766eb
ueki yoshihiro <pirori67@gmail.com>
2014-07-27 22:31:35 +0900

    TDDでaboutファイル作成完了2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 #の対策
 require 'minitest/autorun'
 
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
