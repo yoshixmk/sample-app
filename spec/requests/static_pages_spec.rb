f85816a6baf224bf625261b4bcc138e619a01c6f
植木善弘 <pirori67@gmail.com>
2015-02-07 00:22:51 +0900

    contactのviewを作っている途中。getからmatchへ

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,60 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-  end
-
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+    it "should have the h1 'Sample App'" do
+      visit root_path
       expect(page).to have_content('Sample App')
     end
+
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
-  describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit help_path
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit help_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
+  describe "About page" do
 
+    it "should have the h1 'About Us'" do
+      visit about_path
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit about_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
