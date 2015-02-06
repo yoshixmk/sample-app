9bae625a23714cbfedd81e77c7237fe29ebc3edb
植木善弘 <pirori67@gmail.com>
2015-02-07 00:56:53 +0900

    rspec test pass. & リファクタリングroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  root  'static_pages#home'
+  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
