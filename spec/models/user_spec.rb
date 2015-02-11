5b9aec3ec147159f3f17e5fb2023af882dab5a8b
植木善弘 <pirori67@gmail.com>
2015-02-11 13:44:08 +0900

    has_secure_passwordとpassword,presenceをコメントアウト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,30 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
-  it { should be_valid } #空文字列以外で通る
+  it { should be_valid } #validationでかからない
 
   describe "when name is not present" do
     before { @user.name = " " }
-    it { should_not be_valid } #空文字列で通る
+    it { should_not be_valid } #validationでかかる
   end
 
   describe "when email is not present" do
     before { @user.email = " " }
-    it { should_not be_valid } #空文字列で通る
+    it { should_not be_valid } #validationでかかる
   end
 
   describe "when name is too long" do
@@ -57,6 +63,19 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 end
