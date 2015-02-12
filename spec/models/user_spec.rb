6d983b6d611cd515dcd64fbf5accd77bc090ac56
植木善弘 <pirori67@gmail.com>
2015-02-12 17:19:29 +0900

    spec.rbを整形

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe User do
 
-
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
@@ -16,16 +15,16 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
 
-  it { should be_valid } #validationでかからない
+  it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " " }
-    it { should_not be_valid } #validationでかかる
+    it { should_not be_valid }
   end
 
   describe "when email is not present" do
     before { @user.email = " " }
-    it { should_not be_valid } #validationでかかる
+    it { should_not be_valid }
   end
 
   describe "when name is too long" do
