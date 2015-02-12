f861a82245ef36d58d04eac965d8da9d7bd618f2
植木善弘 <pirori67@gmail.com>
2015-02-13 01:08:42 +0900

    VALID_EMAIL_REGEXの更新。..を検査

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,4 +98,19 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExaMPle.Com" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  describe "inavalid include .. , using email regexp" do
+    before{ @user.email = "foo@bar..com" }
+    it { should_not be_valid}
+  end
 end
