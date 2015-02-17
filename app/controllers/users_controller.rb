66c3bc96db82638e5433442eec1917d6b8d3a63a
植木善弘 <pirori67@gmail.com>
2015-02-17 09:09:25 +0900

    redirectした。リダイレクトでこけるのを修正中。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
     else
       render 'new'
     end
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
