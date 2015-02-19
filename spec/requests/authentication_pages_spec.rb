00bcb1d613821f2700b6af0701df2327112fbb20
植木善弘 <pirori67@gmail.com>
2015-02-19 17:33:55 +0900

    テストでこけるsignin

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,4 +12,15 @@ describe "AuthenticationPages" do
       it { should have_title('Sign in') }
     end
   end
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+  end
 end
