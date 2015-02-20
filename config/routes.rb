13c305520456f318f34358ed9e95b86e927a75e2
植木善弘 <pirori67@gmail.com>
2015-02-21 00:16:47 +0900

    typo email　など。テストコード修正中

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ SampleApp::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/home',  to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
