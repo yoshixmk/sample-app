d692fb929032a74278312eefa5aa633ff72b0d10
植木善弘 <pirori67@gmail.com>
2015-02-21 00:55:42 +0900

    入れ子のミス修正

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,9 +11,9 @@ describe "Authentication" do
     it { should have_title('Sign in') }
   end
 
-    describe "signin" do
+  describe "signin" do
 
-      before { visit signin_path }
+    before { visit signin_path }
 
     describe "with invalid information" do
       before { click_button "Sign in" }
@@ -32,20 +32,20 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
 
-      describe "with valid information" do
-        let(:user) { FactoryGirl.create(:user) }
-        before do
-          fill_in "Email",    with: user.email.upcase
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
-
-        it { should have_title(user.name) }
-        it { should have_link('Profile', href: user_path(user)) }
-        it { should have_link('Sign out', href: signout_path) }
-        it { should_not have_link('Sign in', href: signin_path) }
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
       end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
     end
   end
 end
