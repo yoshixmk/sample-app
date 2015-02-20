4196d0da235594ca4ec5eace698a8029c4d2f86d
植木善弘 <pirori67@gmail.com>
2015-02-20 09:38:34 +0900

    Invalid email/password combination　の表示をなくす

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーをサインインさせ、ユーザーページ (show) にリダイレクトする。
+    else
+      flash[:error] = 'Invalid email/password combination' # 誤りあり!
+      render 'new'
+    end
   end
 
   def destroy
