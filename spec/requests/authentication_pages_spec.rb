d9a161ee8174e2ed24a014734b290b530fa6a072
植木善弘 <pirori67@gmail.com>
2015-02-23 01:53:21 +0900

    テストコード編集authentication

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,11 +35,11 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      let(:user) { FactoryGirl.create(:user) }
-      before { valid_signin(user) }
+      before { sign_in user }
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       describe "followed by signout" do
@@ -47,16 +47,5 @@ describe "Authentication" do
         it { should have_link('Sign in') }
       end
     end
-  end
-
-  describe "with valid information" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { sign_in user }
-
-    it { should have_title(user.name) }
-    it { should have_link('Profile',     href: user_path(user)) }
-    it { should have_link('Settings',    href: edit_user_path(user)) }
-    it { should have_link('Sign out',    href: signout_path) }
-    it { should_not have_link('Sign in', href: signin_path) }
   end
 end
