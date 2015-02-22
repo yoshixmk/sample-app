08a8ff0b2a0eb75ad52ed78c01ba96af5eaf6433
植木善弘 <pirori67@gmail.com>
2015-02-23 02:39:41 +0900

    テストスイートがパス

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -37,5 +38,11 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Before actions
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
