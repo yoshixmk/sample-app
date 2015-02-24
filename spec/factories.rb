f4ae40d068fcc158b2d197337c69c5c940a1964c
植木善弘 <pirori67@gmail.com>
2015-02-24 10:02:36 +0900

    rake db:resetしてテスト失敗する

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,11 @@ FactoryGirl.define do
     password "foobar"
     password_confirmation "foobar"
   end
+
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
