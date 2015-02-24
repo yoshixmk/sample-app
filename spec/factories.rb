3123883343d83a22369121ee4478c6003d400908
植木善弘 <pirori67@gmail.com>
2015-02-24 10:12:01 +0900

    factories.rbのuser定義の重複をなくしてテストパス

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,5 @@
 FactoryGirl.define do
   factory :user do
-    name "yoshixmk"
-    email "yoshixmk@example.com"
-    password "foobar"
-    password_confirmation "foobar"
-  end
-
-  factory :user do
     sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
