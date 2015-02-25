d7e593b9dc82be142b4e7c79670a5defaab50b94
植木善弘 <pirori67@gmail.com>
2015-02-25 21:15:03 +0900

    順序に関するテストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -132,4 +132,19 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
