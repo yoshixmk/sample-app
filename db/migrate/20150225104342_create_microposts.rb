fd8045f5f75d351a8d15deff96db5cd93b5dc66a
植木善弘 <pirori67@gmail.com>
2015-02-25 20:17:04 +0900

    テストコード追加microposts

--- db/migrate/20150225104342_create_microposts.rb
+++ db/migrate/20150225104342_create_microposts.rb
@@ -4,6 +4,7 @@ class CreateMicroposts < ActiveRecord::Migration
       t.string :content
       t.integer :user_id
 
+
       t.timestamps
     end
     add_index :microposts, [:user_id, :created_at]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225012821) do
+ActiveRecord::Schema.define(version: 20150225104342) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
