07eb28243526af2c4e1e34019b8ebf5d8605221d
植木善弘 <pirori67@gmail.com>
2015-02-25 14:08:10 +0900

    deleteメソッドad

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   private
 
     def user_params
