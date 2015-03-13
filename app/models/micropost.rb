593231c9add932386f147a760f121f15c076b5b2
植木善弘 <pirori67@gmail.com>
2015-03-14 01:00:09 +0900

    そのままのSQL発行

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,8 +5,9 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
 
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
-          followed_user_ids: followed_user_ids, user_id: user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
   end
 end
