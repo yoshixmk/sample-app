a7566a97fda1b1ca19868577d0ed152f52288997
yoshixmk <pirori67@gmail.com>
2021-04-10 14:23:30 +0900

    stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+  ],
+  plugins: [
+    "stylelint-order",
+  ],
+  ignoreFiles: ["**/node_modules/**"],
+  rules: {
+    "string-quotes": "single",
+  },
+};
