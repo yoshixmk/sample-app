9eda08e2e4c65680c3d2bf1ff8f3e105405ee9af
yoshixmk <pirori67@gmail.com>
2021-04-15 23:31:48 +0900

    rm characters

---
+++ src/Counter.tsx
@@ -0,0 +1,33 @@
+import { FC, useState } from 'react';
+import { Card, Statistic, Button } from 'semantic-ui-react';
+
+type Props = {
+};
+
+
+const Counter: FC<Props> = () => {
+  const [count, setCount] = useState(0);
+
+  const reset = (): void => {
+    setCount(0);
+  }
+  const increment = (): void => {
+    setCount(count + 1);
+  }
+
+  return (<>
+    <Card>
+      <Statistic className="number-board">
+        <Statistic.Label>count</Statistic.Label>
+        <Statistic.Value>{count}</Statistic.Value>
+      </Statistic>
+      <Card.Content>
+        <div className="ui two buttons">
+          <Button color="red" onClick={() => reset()}> Reset</Button>
+          <Button color="green" onClick={() => increment()}> +1 </Button>
+        </div>
+      </Card.Content>
+    </Card>
+  </>)
+};
+export default Counter;
