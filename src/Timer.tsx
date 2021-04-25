a2a893e52b9c56da7cd8de06995086d8cd5aaf45
yoshixmk <pirori67@gmail.com>
2021-04-25 18:37:49 +0900

    timer

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,33 +1,27 @@
-import { useEffect, useState, useMemo, VFC, useCallback } from 'react';
+import { VFC } from 'react';
 import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
-import { getPrimes } from './utils/math-tool';
+import useTimer from './hooks/use-timer';
 import './Timer.css';
 
 const Timer: VFC<{ limit: number }> = ({ limit }) => {
-  const [timeLeft, setTimeLeft] = useState(limit);
-  const primes = useMemo(() => getPrimes(limit), [limit]);
-  const reset = useCallback((): void => setTimeLeft(limit), [limit]);
-  const tick = (): void => setTimeLeft((t) => t - 1);
-  useEffect(() => {
-    const timerId = setInterval(tick, 1000);
+  const [timeLeft, isPrime, reset] = useTimer(limit);
 
-    return () => clearInterval(timerId);
-  }, []);
-
-  useEffect(() => {
-    if (timeLeft === 0) reset();
-  }, [timeLeft, reset]);
-
-  return (<Card>
-    <Statistic className="number-board">
-      <Statistic.Label>time</Statistic.Label>
-      <Statistic.Value className={primes.includes(timeLeft) ? 'prime-number' : undefined}>{timeLeft}</Statistic.Value>
-    </Statistic>
-    <Card.Content>
-      <Button color="red" fluid onClick={reset}> <Icon name="redo" />
-  Reset
-          </Button>
-    </Card.Content>
-  </Card>);
+  return (
+    <Card>
+      <Statistic className="number-board">
+        <Statistic.Label>time</Statistic.Label>
+        <Statistic.Value className={isPrime ? 'prime-number' : undefined}>
+          {timeLeft}
+        </Statistic.Value>
+      </Statistic>
+      <Card.Content>
+        <Button color="red" fluid onClick={reset}>
+          <Icon name="redo" />
+          Reset
+        </Button>
+      </Card.Content>
+    </Card>
+  );
 };
+
 export default Timer;
